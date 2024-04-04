fc507ffe888d8bddb5897b93e0c11fd1792f6b49
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-04-04 12:04:49 +0200

    added product to product site

--- app/product-page/[productId]/page.tsx
+++ app/product-page/[productId]/page.tsx
@@ -1,16 +1,41 @@
 "use client";
-import React, { useEffect } from "react";
+
+import React, { useEffect, useState } from "react";
+import { Product } from "@/app/_types/types";
+import Image from "next/image";
 export default function ProductPage(props: any) {
+  const [product, setProduct] = useState<Product | null>(null);
+
   console.log(props);
   console.log(props.params.productId);
+
   useEffect(() => {
     fetch(`https://api.noroff.dev/api/v1/online-shop/${props.params.productId}`)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+      .then((data: Product) => {
+        setProduct(data);
       })
-      .catch((error) => {});
-  }, []);
+      .catch((error) => {
+        console.error("Error fetching product:", error);
+      });
+  }, [props.params.productId]);
 
-  return <div>hei</div>;
+  console.log("denne", product);
+
+  return (
+    <div>
+      {product && (
+        <div key={product.id}>
+          <h3>{product.title}</h3>
+          <Image
+            alt="Image of product"
+            src={product.imageUrl}
+            width={500}
+            height={500}
+          />
+          <p>Price: {product.price}</p>
+        </div>
+      )}
+    </div>
+  );
 }
