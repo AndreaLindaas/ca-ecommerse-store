52df0b2bf0ea0496386f367015287f1e4cdf0c96
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-04-06 22:02:31 +0200

    made possibility to show discounted prices and styling to cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -12,60 +12,73 @@ export default function Cart() {
     console.log("products", products);
 
     return products.map((product: Product) => {
-      const itemTotal = product.price * product.quantity;
+      const itemTotal =
+        product.discountedPrice < product.price
+          ? product.discountedPrice * product.quantity
+          : product.price * product.quantity;
       totalAmount += itemTotal;
       return (
-        <div key={product.id}>
-          <div className="container">
-            <div className="overflow-x-auto">
-              <table className="table">
-                {/* head */}
-                <thead></thead>
-                <tbody>
-                  {/* row 1 */}
-                  <tr>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={product.imageUrl}
-                              alt="image of product"
-                            />
-                          </div>
-                        </div>
-                        <div>
-                          <div className="font-bold">{product.title}</div>
+        <div key={product.id} className="container">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead></thead>
+              <tbody>
+                {/* row 1 */}
+                <tr className="cart-row">
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img src={product.imageUrl} alt="image of product" />
                         </div>
                       </div>
+                      <div>
+                        <div className="font-bold">{product.title}</div>
+                      </div>
+                    </div>
+                  </td>
+                  {product.discountedPrice < product.price ? (
+                    <td className="center">
+                      <div>
+                        <td className="priceDiscount ">{product.price} kr</td>
+                      </div>
+                      <td className=" text-green-700">
+                        {product.discountedPrice} kr
+                        <br />
+                      </td>
                     </td>
-                    <td>
+                  ) : (
+                    <td className="center">
                       {product.price} kr
                       <br />
                     </td>
-                    <td>
-                      <label>
-                        <button
-                          className="btn btn-xs"
-                          onClick={() => removeProduct(product)}
-                        >
-                          -
-                        </button>
-                        {product.quantity}
-                        <button
-                          className="btn btn-xs"
-                          onClick={() => addProduct(product)}
-                        >
-                          +
-                        </button>
-                      </label>
-                    </td>
-                    <th>Total: {itemTotal.toFixed(2)}</th>
-                  </tr>
-                </tbody>
-                {/* foot */}
-              </table>
-            </div>
+                  )}
+
+                  <td className="center">
+                    <label>
+                      <button
+                        className="btn btn-xs"
+                        onClick={() => removeProduct(product)}
+                      >
+                        -
+                      </button>
+                      {product.quantity}
+                      <button
+                        className="btn btn-xs"
+                        onClick={() => addProduct(product)}
+                      >
+                        +
+                      </button>
+                    </label>
+                  </td>
+                  <td className="right">
+                    <strong>Total:</strong> {itemTotal.toFixed(2)},-
+                  </td>
+                </tr>
+              </tbody>
+              {/* foot */}
+            </table>
           </div>
         </div>
       );
@@ -78,13 +91,19 @@ export default function Cart() {
 
       {products.length > 0 && (
         <div>
-          {" "}
-          <div className="font-bold">
-            Total Amount: {totalAmount.toFixed(2)}
+          <div className="font-bold right">
+            Total: {totalAmount.toFixed(2)},-
           </div>
-          <button className="btn" onClick={() => clearCart()}>
+          <button
+            className="btn btn-outline btn-error btn-xs"
+            onClick={() => clearCart()}
+          >
             Clear Cart
           </button>
+          <Link href={"/"}>
+            <button className="btn btn-info ">Shop more</button>
+          </Link>
+          <button className="btn btn-success">Checkout</button>
         </div>
       )}
 
