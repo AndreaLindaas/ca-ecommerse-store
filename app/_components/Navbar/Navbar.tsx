7417972c74eaa9eae781b218cd7ef1e24cfa8aba
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-04-07 14:52:45 +0200

    counter for cart items

--- app/_components/Navbar/Navbar.tsx
+++ app/_components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 "use client";
+import { Product } from "@/app/_types/types";
+import { useCartStore } from "@/app/cartStore";
 import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
+  const { products } = useCartStore();
+
+  const calculateItemsInStore = () => {
+    let items = 0;
+
+    products.forEach((p: Product) => {
+      items = items + p.quantity;
+    });
+    return items;
+  };
   return (
     <nav>
       <ul>
@@ -10,7 +22,7 @@ export default function Navbar() {
           <li>Home</li>
         </Link>
         <Link href="/cart">
-          <li>Cart</li>
+          <li>Cart ({calculateItemsInStore()})</li>
         </Link>
         <Link href="/contact-page">
           <li>Contact Us</li>
